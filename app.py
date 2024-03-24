853babb0c97e341dadd1a88383b1096cadcb45da
Leonardo Barroca <barroca.leonardo@gmail.com>
2024-03-24 11:55:10 -0300

    feat: add /upload route

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
-from flask import Flask, jsonify, request, make_response
+from flask import Flask, jsonify, request, make_response, send_file
 from flask_cors import CORS
+from PIL import Image
+import io
+import cv2
 
 app = Flask(__name__)
 CORS(app)
@@ -10,17 +13,24 @@ message_count = 0
 def get_text():
     return make_response(jsonify("Funcionou!"))
 
+@app.route('/upload', methods=['POST'])
+def upload_image():
+    if 'image' not in request.files:
+        return jsonify({'error': 'Nenhuma imagem encontrada'}), 400
 
-@app.route('/api/message', methods=['GET', 'POST'])
-def manage_message():
-    global message_count
-    if request.method == 'POST':
-        data = request.get_json()
-        message = data.get('message')
-        message_count += 1
-        return jsonify({'message': f'Backend recebeu: {message}, Total de mensagens: {message_count}'})
-    else:
-        return jsonify({'message': f'Olá do backend, Total de mensagens: {message_count}'})
+    image = request.files['image']
+    # Processar a imagem aqui (por exemplo, salvar em disco, processamento de imagem, etc.)
+
+    image_bytes = image.read()  # Lê os bytes da imagem
+    #processed_image_bytes = process_image(image_bytes)  # Função fictícia para processar a imagem
+    processed_image = io.BytesIO(image_bytes)  # Convertendo bytes em um objeto BytesIO
+
+    # Retornando a imagem processada como um arquivo binário
+    return send_file(
+        processed_image,
+        mimetype='image/jpeg'  # Especifique o mimetype correto para o tipo de imagem
+    )
+
 
 if __name__ == '__main__':
     app.run()
